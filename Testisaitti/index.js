253b96f80c5ef9581e94857fb36ea3f67b229ef4
Ilias Doukas <ilias@Iliass-MacBook-Pro.local>
2019-04-26 10:27:24 +0300

    public html

--- Testisaitti/index.js
+++ Testisaitti/index.js
@@ -21,6 +21,13 @@ const options = {
   cert: sslcert
 };
 
+app.use(require('serve-static')(__dirname + '/public'));
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 passport.use(new LocalStrategy(
     (username, password, done) => {
       console.log(`login? ${username}`);
