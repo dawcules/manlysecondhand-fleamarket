85ac1609e77291a025c8700c0a9273212b2fb72c
Ilias Doukas <ilias@Iliass-MacBook-Pro.local>
2019-04-26 09:34:53 +0300

    image table sql

---
+++ SQL/Image.sql
@@ -0,0 +1,6 @@
+create table Image (                  
+    -> id int auto_increment primary key,
+    -> title varchar(100),
+    -> alt varchar(200),
+    -> thumb varchar(200),
+    -> medium(200));
