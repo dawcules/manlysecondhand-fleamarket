218ada5caeb3a3f5d96cf30c15997684a93a2180
Said Haji <svidhaji@gmail.com>
2019-04-26 13:39:27 +0300

    Updated files

---
+++ Testisaitti/upload.js
@@ -0,0 +1,9 @@
+'use strict';
+require('dotenv').config();
+const express = require('express');
+const multer = require('multer');
+const bodyParser = require('body-parser');
+
+const app = Express();
+
+app.
