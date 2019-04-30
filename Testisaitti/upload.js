540d2cdeea37b621ced68b4251c46753f3c0805f
Said Haji <svidhaji@gmail.com>
2019-04-30 09:32:58 +0300

    Pieniä muutoksia

--- Testisaitti/upload.js
+++ Testisaitti/upload.js
@@ -9,6 +9,7 @@ const upload = multer({dest: 'public/uploads/'
 
 app.get('/image', (req, res) => {
   res.sendFile(__dirname + 'index.html');
+  res.json({message: 'Toimiiko?'})
 });
 
 app.post('/image', upload.single('my-img'), (req, res) => {
