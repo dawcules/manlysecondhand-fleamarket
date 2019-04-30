7dfc10aacc6f67a0a7d8f4f2f6bd935c09a8c6a6
Said Haji <svidhaji@gmail.com>
2019-04-30 10:22:38 +0300

    Upload.js updated

--- controller/js/upload.js
+++ controller/js/upload.js
@@ -4,16 +4,25 @@ const express = require('express');
 const multer = require('multer');
 const app = Express();
 
-const upload = multer({dest: 'public/uploads/'
-});
+const upload = multer({dest: 'public/uploads/'});
 
-app.get('/image', (req, res) => {
-  res.sendFile(__dirname + 'index.html');
+app.get('/', (req, res) => {
+  res.sendFile('view/public/index.html');
   res.json({message: 'Toimiiko?'})
 });
 
-app.post('public/uploads', upload.single('my-img'), (req, res) => {
+app.post('public/uploads', upload.multiple('myImages', 10), (req, res, next) => {
+  const files = req.files;
+  if (!files) {
+    const error = new Error('Please choose files');
+    error.httpStatusCode = 400;
+    return next(error)
+  }
+  res.send(files);
 
   res.redirect('/app')
+
 });
+
+
 app.listen(3000);
