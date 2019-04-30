80b3ac24c5dae6c2a5819e11ba42faf5cbd9ecf5
Ilias Doukas <ilias@Iliass-MacBook-Pro.local>
2019-04-30 12:41:54 +0300

    sql20

--- view/index.js
+++ view/index.js
@@ -60,7 +60,8 @@ app.post('/login',
       const unamedata = [req.body.username];
       const useri = dbquery.getusername(unamedata,connection,res);
       const passu = dbquery.getpassword(unamedata,connection,res);
-      console.log(useri + passu);
+      console.log(useri);
+      console.log(passu);
       //passport.authenticate('local', { failureRedirect: 'login.html' }, res);
       if (req.body.username == useri && req.body.password == passu) {
   res.redirect('userpage.html');
