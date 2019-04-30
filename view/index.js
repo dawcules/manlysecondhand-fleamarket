a9c433d744d32d88987f13a144e09b70826e9863
Ilias Doukas <ilias@Iliass-MacBook-Pro.local>
2019-04-30 09:39:34 +0300

    kansiot

--- view/index.js
+++ view/index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const passport = require('passport');
-const db = require('../Utils/DBConnection');
+const db = require('../utils/DBConnection');
 const LocalStrategy = require('passport-local').Strategy;
 
 //Database Connection. use .env or modify DBConnection.js to use your own login information
