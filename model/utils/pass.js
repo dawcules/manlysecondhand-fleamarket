3bfb43612bc9afcdc1aac438b05b06eba0ff3b98
Ilias Doukas <ilias@Iliass-MacBook-Pro.local>
2019-05-02 18:19:03 +0300

    login redone

---
+++ model/utils/pass.js
@@ -0,0 +1,113 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+const mediaTable = require('./media_table');
+const dbq = require('../model/utils/DB_Query');
+
+
+/*passport.use(new LocalStrategy(
+    (username, password, done) => {
+      console.log(`login? ${username}`);
+      // Normally, select * from users where username=?
+      if (dbquery.getusername(username,connection && password==dbquery.getpassword(username,connection)) {
+        return done(null,false);
+      }
+      return done(null, {name: username});
+    }
+));
+
+passport.serializeUser((user, done) => {
+  done (null, user)
+});
+
+passport.deserializeUser((id, done) => {
+  return user;
+});*/
+
+passport.serializeUser((user, done) => {
+  console.log('serialize:', user);
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.use(new LocalStrategy(
+    (username, password, done) => {
+      console.log('Here we go: ' + username);
+      let res = null;
+
+      const doLogin = (username, password) => {
+        return new Promise((resolve, reject) => {
+          dbq.getpassword([username], (result) => {
+            bcrypt.compare(password, result[0], (err, res) => {
+              // res == true
+              if (res) {
+                resolve(result);
+              } else {
+                reject(err);
+              }
+            });
+          });
+        });
+      };
+
+      return doLogin(username, password).then((result) => {
+        if (result.length < 1) {
+          console.log('undone');
+          return done(null, false);
+        } else {
+          console.log('done');
+          delete result[0]; // remove password from user's data before sending it to frontend
+          return done(null, result[0]); // result[0] is user's data, accessible as req.user
+        }
+      }).catch(err => {
+        console.log('err', err);
+      });
+    },
+));
+
+const login = (req, res, next) => {
+  passport.authenticate('local', (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) { // if login not happening
+      return res.sendStatus(403); // http code forbidden
+    }
+    req.logIn(user, (err) => {
+      if (err) {
+        return next(err);
+      }
+      return res.send(req.user); // if login succesful, send user object
+    });
+  })(req, res, next);
+};
+
+// register
+const register = (req, res, next) => {
+  console.log(req.body);
+  const saltRounds = 10;
+  bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+    // Store hash in your password DB.
+    console.log('hash', hash);
+    dbq.registerUser([req.body.username, hash], next);
+  });
+};
+
+// function to check if the user has logged in, to be used in middleware
+const loggedIn = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    console.log(req.user);
+    res.sendStatus(403);
+  }
+};
+
+module.exports = {
+  login: login,
+  register: register,
+  loggedIn: loggedIn,
+};
--- view/index.js
+++ view/index.js
@@ -26,59 +26,10 @@ app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveU
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.post('/login', pass.login);
+app.post('/register', pass.register, pass.login);
 
-/*passport.use(new LocalStrategy(
-    (username, password, done) => {
-      console.log(`login? ${username}`);
-      // Normally, select * from users where username=?
-      if (dbquery.getusername(username,connection && password==dbquery.getpassword(username,connection)) {
-        return done(null,false);
-      }
-      return done(null, {name: username});
-    }
-));*/
 
-passport.serializeUser((user, done) => {
-  done (null, user)
-});
-
-passport.deserializeUser((id, done) => {
-  return user;
-});
-
-app.post('/login',
-    (req, res) => {
-      let passi;
-      const unamedata = [req.body.username];
-      //const useri = dbquery.getusername(unamedata,connection,res);
-      //const passu = dbquery.getpassword(unamedata,connection,res);
-      //console.log(useri);
-      //passport.authenticate('local', { failureRedirect: 'login.html' }, res);
-
-      const loginatt = connection.query('SELECT * FROM User WHERE UserName =?;', unamedata[0], (error, results, fields) => {
-        if (results[0].Password === req.body.password) {
-          console.log();
-        return 'true';
-        }
-        if (error) throw error;
-      });
-      console.log(' Kirjautumistilanne = ' + loginatt);
-
-      if (loginatt == 'true') {
-  res.redirect('userpage.html');
-}
-      else {
-        console.log("Ei toimi login")
-      }
-    });
-
-app.post('/register',
-    (req, res) => {
-    //'INSERT INTO User (UserName, Password, Email, Phone, Location, typeID) VALUES (?, ?, ?, ?, ?, ?);'
-      const data = [req.body.username, req.body.password, req.body.email, req.body.phone, req.body.location, 0];
-      dbquery.insertUser(data, connection, res);
-      console.log(req.body.email);
-    });
 //Setting storage to store the files
 const storage = multer.diskStorage({
   destination: '/.public/uploads/',
