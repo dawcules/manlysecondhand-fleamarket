1e8b039ccce95b0b106b983fb84a6a0bbbeca139
Ilias Doukas <ilias@Iliass-MacBook-Pro.local>
2019-05-03 23:15:03 +0300

    userp use session

--- view/public/js/userp.js
+++ view/public/js/userp.js
@@ -1,5 +1,12 @@
 'use strict';
 
+app.use(session({
+  secret: 'keyboardcat',
+  resave: true,
+  saveUninitialized: true,
+  cookie: {secure: false},
+}));
+
 const uname = req.session.user.UserName;
 
 alert('Hello' + uname);
