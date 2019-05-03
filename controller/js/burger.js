c15bdd3212992528116fe6f7b0a5dbd6e34db8ff
RockRonnie <45205523+RockRonnie@users.noreply.github.com>
2019-05-03 20:57:12 +0300

    New frontpage

--- controller/js/burger.js
+++ controller/js/burger.js
@@ -1,32 +1,22 @@
-'use strict';
-// Select the elements we want to show or hide
-var mobileBtn = document.querySelector('.mobile-btn');
-var menu = document.querySelector('.menu');
-var closeBtn = document.querySelector('.close');
-var overlay = document.querySelector('.overlay');
+const LoginRegister = () =>{
+    const logReg = document.querySelector('.logReg');
+    const form = document.querySelector('.logReg-form');
+    logReg.addEventListener('click', () =>{
+        form.classList.toggle('logReg-form-active');
+    });
+};
+const itemsMenu = () =>{
+    const items= document.querySelector('.items');
+    const menu = document.querySelector('.items-menu');
+    items.addEventListener('click', () =>{
+        menu.classList.toggle('items-menu-active');
+    });
+};
 
 
-// When the user clicks on the hamburger icon the 'open' class is added
-// to both the menu and overlay elements making them visible and triggering the animation
-mobileBtn.addEventListener('click', function(){
-  menu.className += ' open';
-  overlay.className += ' open';
-});
+const app = () =>{
+    LoginRegister();
+    itemsMenu();
+};
 
-
-// When the close button is clicked the 'open' class is removed from
-// both the menu and overlay elements making them invisible
-closeBtn.addEventListener('click', function(){
-  menu.className = 'menu';
-  overlay.className = 'overlay';
-});
-
-// If a user clicks outside the menu on the overlay it will also trigger the event
-// to hide the menu and overlay elements
-window.addEventListener('click', function(event){
-  if(event.target === overlay){
-    menu.className = 'menu';
-    overlay.className = 'overlay';
-  }
-});
-//test
+app();
