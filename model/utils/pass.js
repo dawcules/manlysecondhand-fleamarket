ef8a24013eb980a32dbf25bbb630dbef00ad94e7
Ilias Doukas <ilias@Iliass-MBP.local>
2019-05-05 13:42:39 +0300

    pass finetuning

--- model/utils/pass.js
+++ model/utils/pass.js
@@ -67,10 +67,11 @@ const login = (req, res, next) => {
       console.log('logged in ok');
       //res.redirect('../../view/public/userpage.html');
       //return res.send(req.user); // if login succesful, send user object
-      req.session.user = user;
-      req.session.save();
-      return res.redirect('user');
+      //req.session.user = user;
+      //req.session.save();
+      //return res.redirect('user');
       // KÄYTÄ LIIKENNE INDEX.JS KAUTTA
+      next();
     });
   })(req, res, next);
 };
