d69ced7c74c37ce45dfee48af2dcb3d924e278c6
Ilias Doukas <ilias@Iliass-MacBook-Pro.local>
2019-05-05 14:51:35 +0300

    session test

--- view/index.js
+++ view/index.js
@@ -65,7 +65,7 @@ app.post('/uploads', upload.single('myImages'),(req, res) =>{
 
 app.get('/user', pass.loggedIn, (req, res) => {
   console.log('userp app käyty');
-  console.log(req.session.username);
+  console.log(req.session.user);
 });
 
 /*app.use('/image', (req, res, next) => {
