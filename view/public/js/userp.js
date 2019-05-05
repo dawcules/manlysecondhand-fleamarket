28c8a3b373f63cf7c39b57af3b83cb7d234c92a0
Ilias Doukas <ilias@Iliass-MacBook-Pro.local>
2019-05-05 17:03:56 +0300

    json extract

--- view/public/js/userp.js
+++ view/public/js/userp.js
@@ -1,11 +1,14 @@
 'use strict';
-
+let uname = null;
 alert('Success');
+
 fetch('getsession', {
   credentials: 'include'
 }).then((res) => {
   return res.json();
 }).then((json) => {
   console.log(json);
+  uname = json.UserName;
+  console.log(uname);
   //showHide(json);
 });
