4cbd7aefbf313daad1e4259c61cf1395cffa7013
Ilias Doukas <ilias@Iliass-MacBook-Pro.local>
2019-05-06 22:37:48 +0300

    merge

--- view/public/js/ppage.js
+++ view/public/js/ppage.js
@@ -1,17 +1,17 @@
 'use strict';
 const form = document.querySelector('form');
+const brandButton = document.querySelector('#pbrand');
+const priceButton = document.querySelector('#pprice');
+const condButton = document.querySelector('#pcond');
+const typeButton = document.querySelector('#ptype');
+
+
 const getprd = (evt) => {
 
   evt.preventDefault();
   const brand = document.getElementById('brandname');
   const selected = brand.options[brand.selectedIndex].value;
   console.log('selected brand ' + selected);
-
-
-
-
-
-
 
 
   fetch('getproduct', {
@@ -42,4 +42,31 @@ const getprd = (evt) => {
   });
 };
 
+const showBrand = (evt) => {
+  alert('Tässä merkki')
+};
+
+const showPrice = (evt) => {
+  alert('Tässä hinta')
+};
+
+const showCond = (evt) => {
+  alert('Tässä kunto')
+};
+
+const showType = (evt) => {
+  alert('Tässä tyyppi')
+};
+
+/*<button type="submit" id="pbrand">Brand</button>
+      <button type="submit" id="pprice">Price</button>
+      <button type="submit" id="pcond">Condition</button>
+      <button type="submit" id="ptype">Type</button>*/
+
+
 form.addEventListener('submit', getprd);
+brandButton.addEventListener('click', showBrand);
+priceButton.addEventListener('click', showPrice);
+condButton.addEventListener('click', showCond);
+typeButton.addEventListener('click', showType);
+
