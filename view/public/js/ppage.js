dca3f48f0f869894a309b054110ad89cae376da2
Ilias Doukas <ilias@Iliass-MacBook-Pro.local>
2019-05-06 16:44:08 +0300

    brand selector

--- view/public/js/ppage.js
+++ view/public/js/ppage.js
@@ -1,27 +1,35 @@
 'use strict';
+const form = document.querySelector('form');
+const brand = document.getElementsByName('brandname');
 
-fetch('getproduct', {
-  method: "post",
-  headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({
-    brand: "Nike"
-  })
-}).then((res) => {
-  return res.json();
-}).then((json) => {
-  const pdata = json;
-  const desc = document.createElement('p');
-  const title = document.createElement('h2');
 
-  desc.setAttribute('id','title');
-  desc.innerText = pdata.Description ;
-  title.innerText = pdata.pName;
+const getprd = () => {
+  const brand =
+  fetch('getproduct', {
+    method: "post",
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      brand: brand
+    })
+  }).then((res) => {
+    return res.json();
+  }).then((json) => {
+    const pdata = json;
+    const desc = document.createElement('p');
+    const title = document.createElement('h2');
 
-  const dv = document.querySelector('div');
-  dv.appendChild(desc);
-  dv.appendChild(title);
-  //tänne printit
-});
+    desc.setAttribute('id','title');
+    desc.innerText = pdata.Description ;
+    title.innerText = pdata.pName;
+
+    const dv = document.getElementById('ppage');
+    dv.appendChild(desc);
+    dv.appendChild(title);
+    //tänne printit
+  });
+};
+
+form.addEventListener('submit', getprd);
