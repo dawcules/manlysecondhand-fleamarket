5bfdca93332a261bd8f147a71ef4c0b85cd3e404
Ilias Doukas <ilias@Iliass-MacBook-Pro.local>
2019-05-06 12:17:36 +0300

    resize path /app/

--- view/index.js
+++ view/index.js
@@ -128,7 +128,7 @@ app.get('/getsession', (req, res) => {
 app.get('/getproduct', (req, res) => {
   console.log('1. Funktio alkaa');
   const data = [2];
-  const q = query.selectProductInfo(data, (res);
+  const q = query.selectProductInfo(data, (res));
   console.log('2. queryn jälkeen');
   console.log('3. tulos on ' + q);
   console.log('4. käyttäjä on: ' + q[0]);
