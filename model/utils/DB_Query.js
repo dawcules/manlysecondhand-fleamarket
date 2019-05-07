d9c77f9f67aac5408e8cec2beb5c11243d122d0e
Ilias Doukas <ilias@Iliass-MacBook-Pro.local>
2019-05-07 23:18:42 +0300

    re array

--- model/utils/DB_Query.js
+++ model/utils/DB_Query.js
@@ -129,7 +129,7 @@ const selectProductInfo = (data,callback) => {
     // Used for selecting specific Product information'
     console.log(data);
     connection.query(
-        'SELECT * FROM Product WHERE ?;' //pType = ? AND pBrand = ? AND pCondition BETWEEN ? AND 10 AND Price BETWEEN ? AND ?;',data, pID = *, data, pName,pBrand,Description,Condition,pType,Price,ProductAdded FROM Product
+        'SELECT * FROM Product WHERE pType = 8;' //pType = ? AND pBrand = ? AND pCondition BETWEEN ? AND 10 AND Price BETWEEN ? AND ?;',data, pID = *, data, pName,pBrand,Description,Condition,pType,Price,ProductAdded FROM Product
   ,data,
         (err, results, fields) => {
           console.log('RESULTS' + results);
