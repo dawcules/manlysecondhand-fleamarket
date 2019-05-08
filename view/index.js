dea3c786c307b5ebf97ecdf13bfbb4341fccd2e5
RockRonnie <45205523+RockRonnie@users.noreply.github.com>
2019-05-08 20:08:53 +0300

    D:/Git/

--- view/index.js
+++ view/index.js
@@ -108,10 +108,10 @@ app.use('/image', (req, res, next) => {
     console.log("adding image to the database")
     const data = [
         req.body.title,
-        'view/public/uploads/' + req.file.filename,
+        'uploads/' + req.file.filename,
         req.body.title,
-        'view/public/thumbs/' + req.file.filename,
-        'view/public/medium/' + req.file.filename,
+        'thumbs/' + req.file.filename,
+        'medium/' + req.file.filename,
         2, // dummy product ide
     ];
     query.insertImage(data, res);
