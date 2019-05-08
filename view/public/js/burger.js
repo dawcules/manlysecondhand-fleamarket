d2591babeff2f6f9bfcff3d720bd893588fdbb58
RockRonnie <45205523+RockRonnie@users.noreply.github.com>
2019-05-08 18:34:51 +0300

    bleeh

--- view/public/js/burger.js
+++ view/public/js/burger.js
@@ -20,10 +20,42 @@ const userMenu = () =>{
     });
 };
 
+
+
+
+
+
+// WIP
+const userPage = () =>{
+    const userPage = document.querySelector('.userPage');
+    const userInfo = document.querySelector('.userInfo');
+    userInfo.addEventListener('click', () =>{
+        userPage.classList.toggle('userPage-active');
+    });
+};
+const addProduct = () =>{
+    const addProduct = document.querySelector('.addProduct');
+    const productForm = document.querySelector('.productForm');
+    addProduct.addEventListener('click', () =>{
+        productForm.classList.toggle('productForm-active');
+    });
+};
+
+const productPage = () =>{
+    const loggedIn = document.querySelector('.loggedIn');
+    const userMenu = document.querySelector('.userMenu');
+    loggedIn.addEventListener('click', () =>{
+        userMenu.classList.toggle('userMenu-active');
+    });
+
+};
+
 const app = () =>{
     LoginRegister();
     itemsMenu();
     userMenu();
+    userPage();
+    addProduct();
 };
 
 app();
