a0a1a7f32f629d5596a9ed8066a3e769be691a61
RockRonnie <45205523+RockRonnie@users.noreply.github.com>
2019-05-08 21:40:48 +0300

    blee

--- view/index.js
+++ view/index.js
@@ -78,7 +78,7 @@ app.use('/product', (req, res) => {
                 parseInt(req.body.condition),
                 req.body.ptype,
                 parseInt(req.body.price),
-                parseInt(req.id), // dummy userID
+                parseInt(req.body.id), // dummy userID
                 ];
                 console.log(data);
                 query.insertProduct(data, res);
